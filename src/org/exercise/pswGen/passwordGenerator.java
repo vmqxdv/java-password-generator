f70811aaa14bcbea9c69fc2f1250b611dc86740e
vmqxdv <vquintilio@gmail.com>
2025-07-01 11:18:01 +0200

    feat: Implement basic password generation logic

--- src/org/exercise/pswGen/passwordGenerator.java
+++ src/org/exercise/pswGen/passwordGenerator.java
@@ -1,5 +1,42 @@
 package org.exercise.pswGen;
 
-public class passwordGenerator {
+import java.util.Scanner;
 
+public class passwordGenerator {
+  public static void main(String[] args) {
+    String name;
+    String surname;
+    String favouriteColor;
+    int dayOfBirth;
+    int monthOfBirth;
+    int yearOfBirth;
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println(
+        "Benvenuto nel programma di generazione password!!\nPer ottenere la tua nuova password, compila i campi che ti verranno proposti.\n\n");
+
+    System.out.println("Il tuo nome:");
+    name = in.nextLine();
+
+    System.out.println("Il tuo cognome:");
+    surname = in.nextLine();
+
+    System.out.println("Il tuo colore preferito:");
+    favouriteColor = in.nextLine();
+
+    System.out.println("Il tuo giorno di nascita:");
+    dayOfBirth = in.nextInt();
+
+    System.out.println("Il tuo mese di nascita:");
+    monthOfBirth = in.nextInt();
+
+    System.out.println("Il tuo anno di nascita:");
+    yearOfBirth = in.nextInt();
+
+    int dateNumSum = dayOfBirth + monthOfBirth + yearOfBirth;
+
+    System.out.println("Compilazione completata con successo!! La tua nuova password è:\n"
+        + String.join("-", name, surname, favouriteColor, String.valueOf(dateNumSum)));
+  }
 }
